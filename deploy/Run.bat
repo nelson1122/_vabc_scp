3de33a97d13bf717badcfa79b03dd16ad9fae93d
Nelson Enrique Quemá Taimbud <nquema@bancolombia.com.co>
2023-10-23 19:30:29 -0500

    [Added] v2.5.7 deployment files

---
+++ deploy/Run.bat
@@ -0,0 +1,5 @@
+cd vabc_scp
+set initialize=0
+set localsearch=0
+set multithread=true
+java main/java/Main
---
+++ deploy/Run.sh
@@ -0,0 +1,4 @@
+export initialize=0
+export localsearch=0
+cd vabc_scp/
+java main/java/Main
